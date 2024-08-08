af26d7a6a82dee61d716ae8a7f42a93b8725f352
Javier Roca <103610474+jrocamen@users.noreply.github.com>
2024-08-07 21:00:03 -0500

    1.- Utilizaremos la libreria de SweetAlert para mostrar la alerta de eliminar un usuario. 2.- Eliminaremos filas del DataTable. 3.- Creamos el metodo EnviarCorreo

--- CapaNegocio/CN_Recursos.cs
+++ CapaNegocio/CN_Recursos.cs
@@ -4,16 +4,24 @@ using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Mail;
+using System.Net;
+using System.IO;
 
 namespace CapaNegocio
 {
     public class CN_Recursos
     {
+        public static string GenerarClave()
+        {
+            string clave = Guid.NewGuid().ToString("N").Substring(0, 6);
+            return clave;
+        }
+
         //encriptacion DE TEXTO en SHA256
         public static string ConvertirSha256(string texto)
         {
             StringBuilder Sb = new StringBuilder();
-            //USAR LA REFERENCIA DE "System.Security.Cryptography"
             using (SHA256 hash = SHA256.Create())
             {
                 Encoding enc = Encoding.UTF8;
@@ -24,6 +32,38 @@ namespace CapaNegocio
             }
 
             return Sb.ToString();
+        }
+
+        public static bool EnviarCorreo(string correo, string asunto, string mensaje)
+        {
+            bool resultado = false;
+
+            try
+            {
+                MailMessage mail = new MailMessage();
+                mail.To.Add(correo);
+                mail.From = new MailAddress("rocajavier50@gmail.com");
+                mail.Subject = asunto;
+                mail.Body = mensaje;
+                mail.IsBodyHtml = true;
+
+                var smtp = new SmtpClient()
+                {
+                    Credentials = new NetworkCredential("rocajavier50@gmail.com", "evim pcxw zwjx xkwe\r\n"),
+                    Host = "smtp.gmail.com",
+                    Port = 587,
+                    EnableSsl = true
+                };
+
+                smtp.Send(mail);
+                resultado = true;
+            }
+            catch (Exception ex)
+            {
+                resultado = true;
+            }
+
+            return resultado;
         }
     }
 }
